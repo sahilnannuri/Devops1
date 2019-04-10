c815fd51712b3d2b784d6e8a6ec88f509be2d95d
sahilnannuri <40858298+sahilnannuri@users.noreply.github.com>
2019-04-10 12:46:19 +0530

    Update Hii.java

--- eclipse-workspace2/HelloWorld/src/Hii.java
+++ eclipse-workspace2/HelloWorld/src/Hii.java
@@ -4,6 +4,7 @@ public class Hii {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hiii here it goes");
+		System.out.println("hello every one");
 
 	}
 
